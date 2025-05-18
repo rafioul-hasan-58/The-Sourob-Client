6e7ce8820d2e57c0b33f9c27b38b5ce82aedd075
Rafioul Hasan <rafioulhasan2@gmail.com>
2025-05-18 21:42:10 +0600

    feat: add contact info

--- src/components/modules/others/ContactCard.tsx
+++ src/components/modules/others/ContactCard.tsx
@@ -1,21 +1,19 @@
 import Image from "next/image";
-import contactImg from '../../../assets/contact.png'
-// import { Mail, MapPin, Phone } from "lucide-react";
+import contactImg from '../../../assets/contact.webp'
+import { Mail, MapPin, Phone } from "lucide-react";
 const ContactCard = () => {
     return (
         <div>
-            <div className=" rounded-xl overflow-hidden cursor-pointer mb ">
+            <div className=" rounded-xl overflow-hidden cursor-pointer mb bg-purple-600 px-5 pt-5">
                 <div className="mx-2">
-                    <Image className="h-[450px] w-full" src={contactImg} alt="image" width={450} height={400} />
+                    <Image className="h-[280px] w-[490px] rounded-xl" src={contactImg} alt="image" width={450} height={400} />
                 </div>
-                {/* <h1 className="text-white text-center border-b border-b-purple-500 text-xl lg:mx-36 pb-1.5">Contact Info</h1>
-                <div className=" text-purple-600 text-center flex justify-center">
-                    <div className="space-y-1.5 pt-1.5">
-                        <p className="flex items-center gap-2"><Mail />rafioulhasan2@gmail.com</p>
-                        <p className="flex items-center gap-2"><MapPin />Sreepur,Gazipur</p>
-                        <p className="flex items-center gap-2"><Phone />+8801752966422</p>
-                    </div>
-                </div> */}
+                <h1 className="text-white text-center border-b border-b-purple-500 text-xl lg:mx-36 pb-1.5">Contact Info</h1>
+                <div className="space-y-1.5 px-2 py-5 text-white">
+                    <p className="flex items-center gap-2"><Mail />rafioulhasan2@gmail.com</p>
+                    <p className="flex items-center gap-2"><MapPin />Sreepur,Gazipur</p>
+                    <p className="flex items-center gap-2 "><Phone />+8801752966422</p>
+                </div>
             </div>
         </div>
     );
